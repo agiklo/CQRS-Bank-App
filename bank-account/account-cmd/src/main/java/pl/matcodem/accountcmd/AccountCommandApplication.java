f3235e5b77427c77f8993a57369181d65be3579c
agiklo <mateuszmilewczyk1@gmail.com>
2023-03-20 21:01:21 +0100

    basic project setup

---
+++ bank-account/account-cmd/src/main/java/pl/matcodem/accountcmd/AccountCommandApplication.java
@@ -0,0 +1,13 @@
+package pl.matcodem.accountcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCommandApplication.class, args);
+	}
+
+}
