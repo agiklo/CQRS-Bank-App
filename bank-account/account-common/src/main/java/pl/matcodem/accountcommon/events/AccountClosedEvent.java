5fda99b1cbb995ec5688d52314ebe12e7c3ce855
agiklo <mateuszmilewczyk1@gmail.com>
2023-03-21 19:04:43 +0100

    feat: create an endpoint to close bank account

--- bank-account/account-common/src/main/java/pl/matcodem/accountcommon/events/AccountClosedEvent.java
+++ bank-account/account-common/src/main/java/pl/matcodem/accountcommon/events/AccountClosedEvent.java
@@ -2,11 +2,13 @@ package pl.matcodem.accountcommon.events;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import pl.matcodem.cqrscore.events.BaseEvent;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class AccountClosedEvent extends BaseEvent {
 }
